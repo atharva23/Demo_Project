af3fcfc7cfa8a81e20d290e8d95e6edfc0498378
atharva23 <43331286+atharva23@users.noreply.github.com>
2023-04-05 13:32:16 +0530

    Update excluderules.py

--- Append_Rules/excluderules.py
+++ Append_Rules/excluderules.py
@@ -1,1 +1,19 @@
-required_tags = ['exclude_this_line']
+from cfnlint.rules import CloudFormationLintRule
+from cfnlint.rules import RuleMatch
+
+class ExcludeRule(CloudFormationLintRule):
+    id = 'E0000'
+    shortdesc = 'Exclude specific line'
+    description = 'Exclude a specific line in the CloudFormation template'
+    source_url = ''
+    tags = []
+
+    def match(self, cfn):
+        matches = []
+
+        for idx, line in enumerate(cfn.template.split('\n')):
+            if 'MY-EXCLUDED-LINE' in line:
+                # exclude this line
+                return matches
+
+        return matches
