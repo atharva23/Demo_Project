8b91d5406b9a86d1190fdb10d87d1563d7603266
atharva23 <43331286+atharva23@users.noreply.github.com>
2023-04-05 15:27:38 +0530

    Update skiplinerule.py

--- Append_Rules/skiplinerule.py
+++ Append_Rules/skiplinerule.py
@@ -1,22 +1,19 @@
-import cfnlint.helpers
 from cfnlint.rules import CloudFormationLintRule
 
 
 class SkipLineRule(CloudFormationLintRule):
-    id = 'W9901'
-    shortdesc = 'Skip a specific line'
-    description = 'This rule will skip a specific line in the CloudFormation template'
+    id = 'E9999'
+    shortdesc = 'Skip specific lines in CloudFormation templates'
+    description = 'Skip specific lines in CloudFormation templates'
     source_url = ''
-    tags = ['line-skipping']
+    tags = ['experimental']
 
     def match(self, cfn):
-        matches = []
-        for idx, line in enumerate(cfn.template.split('\n')):
-            if line.strip() == '## SKIP THIS LINE':
-                matches.append(cfnlint.Match(
-                    idx + 1,
-                    0,
-                    self.id,
-                    "This line is skipped"
-                ))
-        return matches
+        filename = cfn.get_filename()
+        if filename.endswith('.yml'):
+            with open(filename, 'r') as f:
+                lines = f.readlines()
+                for i, line in enumerate(lines):
+                    if 'SKIP THIS LINE' in line:
+                        return False
+        return True
