5e45b65943c8095f2c5f30e17a69446b7d3a2b41
atharva23 <43331286+atharva23@users.noreply.github.com>
2023-04-05 13:20:52 +0530

    Update excluderules.py

--- excluderules.py
+++ excluderules.py
@@ -1,12 +1,1 @@
-import cfnlint
-
-def excluderules(rule_id, rule_text, path, **kwargs):
-    template = cfnlint.decode.decode(path)
-    for idx, line in enumerate(template.split('\n')):
-        if 'ExcludeThisLine: true' in line:
-            return cfnlint.Issue(
-                rule_id,
-                rule_text.format(line),
-                cfnlint.ParseError([cfnlint.Match(idx, 0, line)], path),
-            )
-    return None
+required_tags = ['exclude_this_line']
